b3291d8516e95c13c30cf74b4a24b7c0583ee8d0
Aishitei Manalastas <82629444+Christianzgaming@users.noreply.github.com>
2025-02-14 00:42:42 +0800

    Update IAS.cmd

--- IAS.cmd
+++ IAS.cmd
@@ -8,10 +8,10 @@
 ::
 ::   IDM Activation Script (IAS)
 ::
-::   Homepages: https://github.com/lstprjct/IDM-Activation-Script
-::              https://t.me/ModByPiash/5
+::   Homepages: https://github.com/Christianzgaming/activat0r
+::              
 ::
-::       Telegram: @Stripe_op
+::       Facebook: @Chanzkiepogi
 ::
 ::============================================================================
 
@@ -63,7 +63,8 @@ exit /b
 ::========================================================================================================================================
 
 set "blank="
-set "mas=https://github.com/lstprjct/IDM-Activation-Script/wiki/"
+set "mas=https://github.com/Christianzgaming/activat0r/wiki/"
+
 
 ::  Check if Null service is working, it's important for the batch script
 
@@ -370,11 +371,11 @@ if not defined terminal mode 75, 28
 
 echo:
 echo:
-call :_color2 %_White% "             " %_Green% "Create By Piash"
+call :_color2 %_White% "             " %_Green% "Create By ChanZAi"
 echo:            ___________________________________________________ 
 echo:
-echo:               Telegram: @ModByPiash
-echo:               Github: https://github.com/lstprjct
+echo:               Telegram: @ModByChanZAi
+echo:               Github: https://github.com/Christianzgaming
 echo:            ___________________________________________________ 
 echo:                                                               
 echo:               [1] Activate
@@ -392,7 +393,7 @@ choice /C:123450 /N
 set _erl=%errorlevel%
 
 if %_erl%==6 exit /b
-if %_erl%==5 start https://github.com/lstprjct/IDM-Activation-Script & goto MainMenu
+if %_erl%==5 start https://github.com/Christianzgaming/activat0r & goto MainMenu
 if %_erl%==4 start https://www.internetdownloadmanager.com/download.html & goto MainMenu
 if %_erl%==3 goto _reset
 if %_erl%==2 (set frz=1&goto :_activate)
